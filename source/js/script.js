207f4a7be28ed8beea791acb5dbfb7ff1bfc2191
Lastavenka <natasha.lastavenka@gmail.com>
2016-09-20 15:35:56 +0300

    Fix js

--- source/js/script.js
+++ source/js/script.js
@@ -3,7 +3,7 @@
     'use strict';
 
     var file     = 'img/sprite.svg',
-        revision = 1;
+        revision = 3;
 
     if( !document.createElementNS || !document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect )
         return true;
